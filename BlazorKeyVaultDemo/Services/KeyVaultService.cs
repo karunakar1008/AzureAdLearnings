7ac6ea8766b9bb973ce28c1f2f690556bcb9e292
Bhogyari Karunakar <kbhogyari@@deloitte.com>
2025-07-01 18:21:23 +0530

    Implement key vault for storing app registtation secret key

---
+++ BlazorKeyVaultDemo/Services/KeyVaultService.cs
@@ -0,0 +1,20 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace BlazorKeyVaultDemo.Services
+{
+    public class KeyVaultService
+    {
+        private readonly SecretClient _secretClient;
+        public KeyVaultService(string keyVaultUrl)
+        {
+            _secretClient = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+        }
+
+        public async Task<string> GetSecretAsync(string secretName)
+        {
+            KeyVaultSecret secret = await _secretClient.GetSecretAsync(secretName);
+            return secret.Value;
+        }
+    }
+}
---
+++ BlazorKeyVaultDemo/Services/KeyVaultService2.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace BlazorKeyVaultDemo.Services
+{
+    public class KeyVaultService2
+    {
+        private readonly SecretClient _secretClient;
+        private readonly IConfiguration configuration;
+        private string keyVaultUrl;
+        public KeyVaultService2(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            keyVaultUrl = $"https://{this.configuration["KeyVaultName"]}.vault.azure.net/";
+            _secretClient = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+
+        }
+        public async Task<string> GetSecretAsync(string secretName)
+        {
+            KeyVaultSecret secret = await _secretClient.GetSecretAsync(secretName);
+            return secret.Value;
+        }
+    }
+}
