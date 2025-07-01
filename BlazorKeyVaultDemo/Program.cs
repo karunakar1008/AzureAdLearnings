aa51f7cc71e6927aa4d399ed9bf657c6dd2daef2
Bhogyari Karunakar <kbhogyari@@deloitte.com>
2025-07-01 19:11:30 +0530

    Implement azure ad login

--- BlazorKeyVaultDemo/Program.cs
+++ BlazorKeyVaultDemo/Program.cs
@@ -3,6 +3,7 @@ using BlazorKeyVaultDemo.Components;
 using BlazorKeyVaultDemo.Services;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 
 namespace BlazorKeyVaultDemo
 {
@@ -16,22 +17,23 @@ namespace BlazorKeyVaultDemo
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
 
+          
             // Load Key Vault URL from configuration or hardcode here
             string keyVaultUrl = $"https://{builder.Configuration["KeyVaultName"]}.vault.azure.net/"; // <-- your key vault URL here
 
             // Create service to fetch secrets from Key Vault
             var keyVaultService = new KeyVaultService(keyVaultUrl);
 
-            
-
             // Fetch client secret synchronously (acceptable during startup)
             string clientSecret = keyVaultService.GetSecretAsync("AzureADClientSecret").GetAwaiter().GetResult();
 
+            var configuration = builder.Configuration;
 
             //Different way of implementing
-            var keyVaultService2 = new KeyVaultService2(builder.Configuration);
-            string clientSecret2 = keyVaultService.GetSecretAsync("AzureAd--ClientSecret").GetAwaiter().GetResult();
-
+            //var keyVaultService2 = new KeyVaultService2(builder.Configuration);
+            //string clientSecret2 = keyVaultService.GetSecretAsync("AzureAd--ClientSecret").GetAwaiter().GetResult();
+            //builder.Services.AddSingleton<KeyVaultService>();
+            
             builder.Services.AddSingleton<KeyVaultService>();
             var initialScopes = builder.Configuration.GetValue<string>("DownstreamApi:Scopes")?.Split(' ');
 
@@ -48,7 +50,29 @@ namespace BlazorKeyVaultDemo
                                      .AddMicrosoftGraph(builder.Configuration.GetSection("DownstreamApi:Scopes"))
                                    .AddInMemoryTokenCaches();
 
+
+            builder.Services.AddAuthorization(options =>
+            {
+                // By default, all incoming requests will be authorized according to the default policy
+                //options.FallbackPolicy = options.DefaultPolicy;
+            });
+
+            builder.Services.AddControllersWithViews()
+          .AddMicrosoftIdentityUI();
+
+            //Learning: You can define here or Router component with <CascadingAuthenticationState>
+            //builder.Services.AddCascadingAuthenticationState();
+
+            builder.Services.AddRazorPages();
+
+            //// Add consent handler
+            //builder.Services.AddServerSideBlazor()
+            //    .AddMicrosoftIdentityConsentHandler();
+
             var app = builder.Build();
+
+            var logger = app.Services.GetRequiredService<ILogger<Program>>();
+            logger.LogInformation($"Key vault secret:{clientSecret}");
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
@@ -58,10 +82,16 @@ namespace BlazorKeyVaultDemo
                 app.UseHsts();
             }
 
+
             app.UseHttpsRedirection();
 
             app.UseStaticFiles();
             app.UseAntiforgery();
+
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.MapControllers();
 
             app.MapRazorComponents<App>()
                 .AddInteractiveServerRenderMode();
